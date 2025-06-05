b33d6dcd102b43d5dcb0aff383ad4d4ff484a778
Revzov-Boris <boria.revazov@yandex.ru>
2025-06-06 01:03:51 +0300

    исправление id в Position, добавление ERD

--- src/main/java/src/entities/Position.java
+++ src/main/java/src/entities/Position.java
@@ -13,22 +13,23 @@ import lombok.Setter;
 @Entity
 @Table(name = "positions")
 public class Position {
-    @Id
-    private Long id;
+    @EmbeddedId
+    private PositionId positionId;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @MapsId  // Берет id из cheque
-    @JoinColumn(name = "cheque_id")  // Столбец id (и PK, и FK)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("chequeId")
+    @JoinColumn(name = "cheque_id", referencedColumnName = "id")
     private Cheque cheque;
 
-    @ManyToOne
-    @JoinColumn(name = "medicine_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("medicineId")
+    @JoinColumn(name = "medicine_id", referencedColumnName = "id")
     private Medicine medicine;
 
     private short quantity;
 
     @Override
     public String toString() {
-        return String.format("%s | %s | %s", cheque.getId(), medicine.getId(), quantity);
+        return String.format("%s | %s | %s", positionId.getChequeId(), positionId.getMedicineId(), quantity);
     }
 }
---
+++ src/main/java/src/entities/PositionId.java
@@ -0,0 +1,28 @@
+package src.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Embeddable
+public class PositionId implements Serializable {
+    @Column(name = "cheque_id")
+    private Long chequeId;
+
+    @Column(name = "medicine_id")
+    private Long medicineId;
+
+    @Override
+    public String toString() {
+        return String.format("%s | %s", chequeId, medicineId);
+    }
+}
